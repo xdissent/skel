edba37221f29e3b2db005495b5b38f2a78c284ae
Greg Thornton <xdissent@gmail.com>
2009-02-16 22:44:09 +0000

    loads of js work. about to reorganize.

--- skel/static/js/superimage.js
+++ skel/static/js/superimage.js
@@ -2,170 +2,103 @@
 (function($) {
 
 $.widget('ui.superimage', {
-
-    _new_thumb_counter: 0,
-    
-    
-    _current_thumb: false,    
-    
     
     _init: function() {
         this._setup_image(true);
     },
     
-    
-    _edit_thumb: function(thumb_container) {
-        var self = this;
-        
-        // turn off current edit
-        if (self._current_thumb) {
-            self._current_thumb.find('.edit').click();
-        }
-        // turn on form field so changes count
-        thumb_container.find('input[disabled]').not('[name*=delete]').removeAttr('disabled');
-        // if there's a thumb image, hide it
-        // focus the title
-        // animate selection to current coords
-        // move slider to width and height
-        self._current_thumb = thumb_container;
-    },
-    
-
-    _add_row: function() {
-        var self = this;
-        
-        $new = self.$widget.find('.ui-superimage-thumbnail-template').clone();
-        $new.find('.ui-superimage-thumbnail-coords').show().find('input');
-        $new.removeClass('ui-superimage-thumbnail-template').remove();
-        $new.find('[id$=_template]').each(function() {
-            var $this = $(this);
-            var new_id = $this.attr('id').replace('_template', '_new' + self._new_thumb_counter);
-            $this.attr({id: new_id});
-        });
-        $new.find('[name$=_template]').each(function() {
-            var $this = $(this);
-            var new_name = $this.attr('name').replace('_template', '_new' + self._new_thumb_counter);
-            $this.attr({name: new_name});
-        });
-        $new.find('[for$=_template]').each(function() {
-            var $this = $(this);
-            var new_for = $this.attr('for').replace('_template', '_new' + self._new_thumb_counter);
-            $this.attr('for', new_for);
-        });
-        
-        $new.find('.ui-superimage-thumbnail-edit-delete .delete').toggle(function() {
-            $(this).text('Undelete');
-            $(this).closest('.ui-superimage-thumbnail-container').find('input').attr({disabled: true});
-            return false;
-        }, function() {
-            $(this).text('Delete');
-            $(this).closest('.ui-superimage-thumbnail-container').find('input').removeAttr('disabled');
-            return false;
-        });
-        
-        $new.find('.ui-superimage-thumbnail-edit-delete .edit').toggle(function() {
-            $(this).text('Done Editing');
-            self._edit_thumb($(this).closest('.ui-superimage-thumbnail-container'));
-            return false;
-        }, function() {
-            // deactivate fields etc
-            $(this).text('Edit');
-            self._current_thumb = false;
-            return false;
-        });
-        
-        self.$widget.find('.ui-superimage-thumbnails > div:first-child').append($new);
-        
-        self._new_thumb_counter++;
-        
-        $new.find('.ui-superimage-thumbnail-edit-delete .edit').click();
-        
-        $new.find('input[name*=_title]').focus();
-    },
-
-    
     _setup_image: function(init) {
         var self = this, o = this.options;
         
-        self.$widget = self.element.closest('.ui-superimage-widget');
-        self.$source = self.$widget.find('.ui-superimage-source');
-        self.$source.tabs();
+        self.id_prefix = this.element.attr('id') + '-' + self.widgetBaseClass;
+        
+        self.original_size = [this.element.width(), this.element.height()];
+        console.log(self.original_size);
+        self.original_size = [640, 480];
                 
-        self.element.Jcrop({
-            onChange: function(c) {
-                self._update_coords(c);
+        self.$widget = self.element.wrap('<div class="ui-superimage-widget"></div>').parent();
+        
+        self.$original = $('<div class="' + self.widgetBaseClass + '-original"><ul><li><a href="#' + self.id_prefix +
+            '-current" class="' + self.widgetBaseClass + '-current-tab" title="Current Image"><span>Current</span></a></li>' +
+            '<li><a href="#' + self.id_prefix + '-upload" class="' + self.widgetBaseClass + '-upload-tab" title="Upload">' +
+            '<span>Upload</span></a></li>' + '</ul>'+
+            '<div id="' + self.id_prefix + '-current" class="' + self.widgetBaseClass + '-current"></div>' +
+            '<div id="' + self.id_prefix + '-upload" class="' + self.widgetBaseClass + '-upload"><p>upload</p></div>' +
+        '</div>');
+        self.$plugins = $('<div class="' + self.widgetBaseClass + '-plugins"><ul></ul></div>');
+
+        self.$widget.append(self.$original);
+        self.$widget.append(self.$plugins);
+            
+        self.$original.find('.ui-superimage-current').append(this.element);
+        this.element.css({width: '100%'});
+
+
+    
+        self.$widget.splitpane({
+            resize: function(e, ui) {
+                self._trigger('resize', null, self._ui());
             },
-            onSelect: function(c) {
-                self._update_coords(c);
-            },
         });
         
-        self._fix_django();
-        
-        self.$widget.find('.ui-superimage-thumbnail-add').click(function() {
-            try {
-                        self._add_row();
-
-            } catch(e) {
-                console.log(e.description);
-            }
-            return false;
+        $.each(o.plugins, function() {
+            self.$plugins.find('ul').append(
+                '<li><a href="#' + self.id_prefix  + '-' + this.id + 
+                '" class="' + self.widgetBaseClass + '-' + this.id + '-tab" title="' + 
+                this.label + '"><span>' + this.label + '</span></a></li>'
+            ).after(
+                '<div id="' + self.id_prefix  + '-' + this.id + '"></div>'
+            );
+            this._init(self._ui());
         });
+        self._trigger('plugin_init', null, self._ui());
         
-        self.$widget.find('.ui-superimage-thumbnail-edit-delete .delete').toggle(function() {
-            $(this).text('Undelete');
-            $(this).next('input').removeAttr('disabled');
-            return false;
-        }, function() {
-            $(this).text('Delete');
-            $(this).next('input').attr({disabled: 'disabled'});
-            return false;
-        });
+        self.$original.tabs();
+        self.$plugins.tabs();
         
-        self.$widget.find('.ui-superimage-thumbnail-edit-delete .edit').toggle(function() {
-            $(this).text('Done Editing')
-            self._edit_thumb($(this).closest('.ui-superimage-thumbnail-container'));
-            return false;
-        }, function() {
-            $(this).text('Edit');
-            self._current_thumb = false;
-            return false;
-        });
-        
-        self.$widget.children(':first').resizable({
-            maxWidth: self.$widget.width(),
-            minWidth: self.$widget.width(),
-            minHeight: self.$widget.height(),
-        });
     },
-    
-
-    _fix_django: function() {
-        this.$widget.prev('label').hide();
-    },
-    
-    
-    _update_coords: function(c) {
-        var self = this;
-                    console.log(self._current_thumb);
-        if (self._current_thumb) {
-            console.log('updating');
-            $coords = self._current_thumb.find('.ui-superimage-thumbnail-coords');
-
-            $.each({
-                x1: c.x,
-                y1: c.y,
-                x2: c.x2,
-                y2: c.y2,
-                 w: c.w,
-                 h: c.h,
-            }, function (name, val) {
-                $(self._current_thumb.find('[name*=thumbnail_' + name + ']').val(val))
-            });
-        }
-    }
-    
+        
+    _ui: function() {
+        return {
+            widget: this.$widget,
+            element: this.element,
+            id_prefix: this.id_prefix,
+            prefix: this.widgetBaseClass,
+            original_size: this.original_size,
+        };
+    }    
 });
+
+
+var thumbnail = {
+    id:     'thumbnail',
+    label:  'Thumbnails',
+    _init:  function(ui) {
+    
+        $(ui.element).wrap('<div></div>').crop({ original_size: ui.original_size });
+        var image = ui.element;
+        ui.widget.bind('resize', function(e, ui) {
+            image.crop('resize');
+        });
+    },
+};
+
+$.extend($.ui.superimage, {
+    getter: '',
+    version: '0.1',
+    eventPrefix: 'superimage',
+    defaults: {
+        plugins: [
+            thumbnail,
+            {   id:     'resize',
+                label:  'Resize',
+                _init:  function(superimage) {
+                },
+            },
+        ]
+    }
+});
+
 
 $(function() {
     $('.ui-superimage').superimage();
--- skel/urls.py
+++ skel/urls.py
@@ -12,6 +12,11 @@ urlpatterns = patterns('',
     url(r'^portfolio/', include('skel.portfolio.urls')),
     url(r'^users/', include('skel.accounts.urls')),
     url(r'^markupeditor/', include('skel.markupeditor.urls')),
+    url(r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
+    url(r'^superimage/$', 'django.views.generic.simple.direct_to_template', {'template': 'superimage.html'}),
+    url(r'^splitpane/$', 'django.views.generic.simple.direct_to_template', {'template': 'splitpane.html'}),
+    url(r'^crop/$', 'django.views.generic.simple.direct_to_template', {'template': 'crop.html'}),
+
 )
 
 if settings.DEBUG:
