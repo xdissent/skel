8c2878b33efa0192bada42f8fae3a8093655fab4
Greg Thornton <xdissent@gmail.com>
2009-02-23 23:42:37 +0000

    my local_settings.py

--- skel/local_settings.py
+++ skel/local_settings.py
@@ -1,18 +1,18 @@
-#import os
+import os
 
-#PROJ_PATH = os.path.abspath(os.path.dirname(__file__))
-#PROJ_NAME = os.path.basename(os.path.dirname(__file__))
+PROJ_PATH = os.path.abspath(os.path.dirname(__file__))
+PROJ_NAME = os.path.basename(os.path.dirname(__file__))
 
-#VIRTUAL_ENVIRONMENT_PATH = '/home/36218/containers/django/mt_virtualenvs/hartzog_skel'
+VIRTUAL_ENVIRONMENT_PATH = '/Users/xdissent/.virtualenvs/hartzog_skel'
 
-#DEBUG = True
-#TEMPLATE_DEBUG = True
+DEBUG = True
+TEMPLATE_DEBUG = True
 
-#DATABASE_ENGINE = 'sqlite3'
-#DATABASE_NAME = os.path.join(PROJ_PATH, 'sqlite.db')
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME = os.path.join(PROJ_PATH, 'sqlite.db')
 #DATABASE_USER = ''
 #DATABASE_PASSWORD = ''
 #DATABASE_HOST = ''
 #DATABASE_PORT = ''
 
-#CACHE_BACKEND = 'locmem:///?timeout=30&max_entries=200'
+CACHE_BACKEND = 'locmem:///?timeout=30&max_entries=200'
