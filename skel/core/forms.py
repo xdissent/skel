838548442bd85f9751b531775375ee79c79c4551
Greg Thornton <xdissent@gmail.com>
2009-06-26 00:58:05 +0000

    Merging from xdissent branch

--- skel/core/forms.py
+++ skel/core/forms.py
@@ -4,21 +4,16 @@ from django.contrib.flatpages.admin import FlatpageForm
 from skel.core import settings
 
 
-if settings.CORE_MARKUP_COMMENTS:
-    from skel.markupeditor.markups import get_choices
-    from skel.markupeditor.fields import MarkupEditorWidget
-    
-    choices = [('', '---------')] + get_choices()
-    comment_field = forms.CharField(widget=MarkupEditorWidget)
-    markup_field = forms.TypedChoiceField(choices=choices, required=False)
-else:
-    comment_field = forms.CharField()
-    markup_field = None
+
+from skel.markupeditor.markups import get_choices
+from skel.markupeditor.fields import MarkupEditorWidget
+
+choices = [('', '---------')] + get_choices()
     
 
 class SkelCommentForm(CommentForm):
-    comment = comment_field
-    comment_markup = markup_field
+    comment = forms.CharField(widget=MarkupEditorWidget)
+    comment_markup = forms.TypedChoiceField(choices=choices, required=False)
     
     def get_comment_model(self):
         from skel.core.models import SkelComment
@@ -26,16 +21,9 @@ class SkelCommentForm(CommentForm):
 
     def get_comment_create_data(self):
         data = super(SkelCommentForm, self).get_comment_create_data()
-        if settings.CORE_MARKUP_COMMENTS:
-            data['comment_markup'] = self.cleaned_data['comment_markup']
+        data['comment_markup'] = self.cleaned_data['comment_markup']
         return data
 
-
-if settings.CORE_MARKUP_FLATPAGES:        
-    from skel.markupeditor.fields import MarkupEditorWidget
-    flatpage_content_field = forms.CharField(widget=MarkupEditorWidget)
-else:
-    flatpage_content_field = forms.CharField()
     
 class SkelFlatpageForm(FlatpageForm):
-    content = flatpage_content_field
+    content = forms.CharField(widget=MarkupEditorWidget)
