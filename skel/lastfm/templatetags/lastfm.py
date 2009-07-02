52248f89fb079b0ae239e9ea54a42520294fc5d9
Greg Thornton <xdissent@gmail.com>
2009-07-02 19:11:51 +0000

    Merging feedcache-django and Last.fm from xdissent

--- skel/lastfm/templatetags/lastfm.py
+++ skel/lastfm/templatetags/lastfm.py
@@ -1,15 +1,18 @@
-from django.conf import settings
 from django import template
-from time import mktime, localtime
+from time import mktime
 from datetime import datetime
-import feedparser
+from django.core.cache import cache
+from feedcache.cache import Cache
+from feedcache.cachedjango import CacheDjango
 
 register = template.Library()
 
 @register.inclusion_tag('lastfm/recent_tracks.html')
 def show_recent_tracks(lastfm_user):
-    d = feedparser.parse("http://ws.audioscrobbler.com/1.0/user/%s/recenttracks.rss" % lastfm_user)
-    tracks = d.entries[:15]
+    storage = CacheDjango(cache)
+    fc = Cache(storage)
+    d = fc.fetch("http://ws.audioscrobbler.com/1.0/user/%s/recenttracks.rss" % lastfm_user)
+    tracks = d.entries
     for track in tracks:
         track.updated = datetime.fromtimestamp(mktime(track.updated_parsed))
         (track.artist, track.title) = track.title.split(u' \u2013 ', 1)
