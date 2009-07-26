081ccb17da5b4b8c2b916e9d6a4780c233c18792
Greg Thornton <xdissent@gmail.com>
2009-07-26 03:18:09 +0000

    Added Skel import hack for running from paver command

--- pavement.py
+++ pavement.py
@@ -3,18 +3,22 @@ from paver.easy import *
 # import paver.doctools
 from paver.setuputils import setup, find_package_data, find_packages
 
-sys.path.append('/Users/xdissent/Sites/hartzog_skel')
+try:
+    import skel
+except ImportError:
+    sys.path.insert(0, '')
+    
 from skel.core.management.tasks import install_skel_tasks
 install_skel_tasks()
 
 package_data = find_package_data('skel', package='skel', 
                                  only_in_packages=False)
 packages = find_packages()
-data_files = ['requirements.txt']
+data_files = []
 
-print package_data
-print packages
-print data_files
+# print package_data
+# print packages
+# print data_files
 
 setup(
     name='Skel',
