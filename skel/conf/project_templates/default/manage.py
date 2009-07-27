7d1b1eeee953c8a0e2928ccc5a32bb83ce2ebd65
Greg Thornton <xdissent@gmail.com>
2009-07-27 14:29:45 +0000

    Removing old stuff

--- skel/conf/project_templates/default/manage.py
+++ skel/conf/project_templates/default/manage.py
@@ -38,6 +38,7 @@ if settings is None:
         sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
         sys.exit(1)
 
+# This might be redundant.
 from django.core.management import setup_environ
 setup_environ(settings)
 
