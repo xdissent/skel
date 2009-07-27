4649dbf0a82d642abf103f8b9938e356b11332ad
Greg Thornton <xdissent@gmail.com>
2009-07-27 14:09:59 +0000

    Namespaced blog settings

--- skel/blog/settings.py
+++ skel/blog/settings.py
@@ -1,28 +1,27 @@
 from django.conf import settings
 
+SKEL_BLOG_MARKUP_ENABLED = ('skel.markupeditor' in settings.INSTALLED_APPS)
 
-BLOG_MARKUP_ENABLED = ('skel.markupeditor' in settings.INSTALLED_APPS)
+SKEL_BLOG_MEDIA_ENABLED = ('massmedia' in settings.INSTALLED_APPS)
 
-BLOG_MEDIA_ENABLED = ('massmedia' in settings.INSTALLED_APPS)
+SKEL_BLOG_TAGS_ENABLED = ('tagging' in settings.INSTALLED_APPS)
 
-BLOG_TAGS_ENABLED = ('tagging' in settings.INSTALLED_APPS)
+SKEL_BLOG_CATEGORIES_ENABLED = ('skel.categories' in settings.INSTALLED_APPS)
 
-BLOG_CATEGORIES_ENABLED = ('skel.categories' in settings.INSTALLED_APPS)
+SKEL_BLOG_COMMENTS_ENABLED = ('django.contrib.comments' in settings.INSTALLED_APPS)
 
-BLOG_COMMENTS_ENABLED = ('django.contrib.comments' in settings.INSTALLED_APPS)
+SKEL_BLOG_AUTO_CLOSE_COMMENTS_DAYS = 60
 
-BLOG_AUTO_CLOSE_COMMENTS_DAYS = 60
+SKEL_BLOG_FEEDS_ENABLED = True
 
-BLOG_FEEDS_ENABLED = True
+SKEL_BLOG_FEED_TITLE = 'Skel Blog Feed'
 
-BLOG_FEED_TITLE = 'Skel Blog Feed'
+SKEL_BLOG_FEED_DESCRIPTION = 'Skel Blog Feed'
 
-BLOG_FEED_DESCRIPTION = 'Skel Blog Feed'
+SKEL_BLOG_FEED_NUM_ITEMS = 10
 
-BLOG_FEED_NUM_ITEMS = 10
+SKEL_BLOG_FEED_SHOW_AUTHOR_EMAIL = False
 
-BLOG_FEED_SHOW_AUTHOR_EMAIL = False
+SKEL_BLOG_CATEGORY_FEED_TITLE = '%s - %%(category.title)s Category' % SKEL_BLOG_FEED_TITLE
 
-BLOG_CATEGORY_FEED_TITLE = '%s - %%(category.title)s Category' % BLOG_FEED_TITLE
-
-BLOG_TAG_FEED_TITLE = '%s - Entries tagged "%%(tag.name)s"' % BLOG_FEED_TITLE
+SKEL_BLOG_TAG_FEED_TITLE = '%s - Entries tagged "%%(tag.name)s"' % SKEL_BLOG_FEED_TITLE
