3252d549ae49f37b058edb9c2dd802d3edcfeb0d
Greg Thornton <xdissent@gmail.com>
2009-07-27 02:31:07 +0000

    renaming django to djangosupport in management

--- bin/skel-admin.py
+++ bin/skel-admin.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from skel.core import management
+from skel.core.management.djangosupport import execute_from_command_line
 
 if __name__ == "__main__":
-    management.execute_from_command_line()
+    execute_from_command_line()
