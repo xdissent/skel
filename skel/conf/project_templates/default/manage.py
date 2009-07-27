0f780d4d356fb2bfbcd53a9e7dbc40e549807d97
Greg Thornton <xdissent@gmail.com>
2009-07-27 02:31:17 +0000

    renaming django to djangosupport in management

--- skel/conf/project_templates/default/manage.py
+++ skel/conf/project_templates/default/manage.py
@@ -40,6 +40,6 @@ if settings is None:
 
 from django.core.management import setup_environ
 setup_environ(settings)
-from skel.core.management import execute_manager
+from skel.core.management.djangosupport import execute_manager
 if __name__ == "__main__":
     execute_manager(settings)
