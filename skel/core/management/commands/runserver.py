4b54327c77811d65e80f38d8fcd41beadfefb9bf
Greg Thornton <xdissent@gmail.com>
2009-07-27 14:18:41 +0000

    Removing fake runserver temporarily

--- skel/core/management/commands/runserver.py
+++ skel/core/management/commands/runserver.py
@@ -5,7 +5,7 @@ import subprocess
 import time
 import sys
 
-class Command(runserver.Command):    
+class BrokenCommand(runserver.Command):    
     def handle(self, *args, **kwargs):
         server_proc = subprocess.Popen(['python -m smtpd -n -c DebuggingServer localhost:1025'], shell=True)
         open_proc = subprocess.Popen(['open http://localhost:8000/'], shell=True)
