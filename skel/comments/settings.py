bbeca5d06fbe536e2d526d59e8634c8d29eef0f3
Greg Thornton <xdissent@gmail.com>
2009-07-27 14:42:01 +0000

    Cleanup

--- skel/comments/settings.py
+++ skel/comments/settings.py
@@ -1,5 +1,11 @@
 from django.conf import settings
 
-COMMENTS_MARKUP = ('django.contrib.comments' in settings.INSTALLED_APPS)
+# Determine whether or not to accept marked up comments.
+SKEL_COMMENTS_MARKUP = ('django.contrib.comments' in settings.INSTALLED_APPS and
+                        'skel.markupeditor' in settings.INSTALLED_APPS)
 
-COMMENTS_ENABLED_DEFAULT = True
+# If ``COMMENTS_ENABLED_DEFAULT`` is a boolean value, commenting will be
+# enabled (True) or disabled (False) by default for all models. If a tuple 
+# of model names in the form ``app.model`` are provided, those models will
+# have comments enabled by default while all others will not.
+SKEL_COMMENTS_ENABLED_DEFAULT = True
