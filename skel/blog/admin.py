4e32b471f80aee5ad976514be50a80c71abcf769
Greg Thornton <xdissent@gmail.com>
2009-08-08 00:12:34 +0000

    Added new markup and categories to blog.

--- skel/blog/admin.py
+++ skel/blog/admin.py
@@ -1,65 +1,19 @@
 from django.contrib import admin
-from skel.blog import settings
+from skel.markup.admin import MarkedUpAdmin
 from skel.blog.models import Entry
-from skel.markupeditor.fields import MarkupEditorField, MarkupEditorWidget
+from skel.blog import settings
 
 
-entry_fields = [
-    'title', 
-    'public',
-    'author',
-    'sites',
-    'summary',
-    'body',
-]
-
-entry_list_filter = ('author', 'public', 'sites')
-
-
-# if settings.BLOG_TAGS_ENABLED:
-entry_fields.append('tags')
-entry_list_filter += ('tags',)
-
-
-# if settings.BLOG_CATEGORIES_ENABLED:
-entry_fields.append('categories')
-entry_list_filter += ('categories',)
-
-
-# if settings.BLOG_MARKUP_ENABLED:
-for field in ('summary', 'body'):
-    entry_fields.insert(entry_fields.index(field) + 1, field + '_markup')
-        
-
-# if settings.BLOG_MEDIA_ENABLED:
-entry_fields.append('media')
-
-
-class EntryAdmin(admin.ModelAdmin):
+class EntryAdmin(MarkedUpAdmin):
     prepopulated_fields = {'slug': ('title',)}
-    fieldsets = (
-        (None, {
-            'fields': entry_fields
-        }),
-        ('Advanced options', {
-            'classes': ('collapse',),
-            'fields': (
-                'slug',
-                'published',
-            )
-        }),
-    )
     list_display = ('published', 'title', 'author', 'public',)
-    list_filter = entry_list_filter
     search_fields = ['title', 'body', 'summary']
-    model_admin_manager = Entry.admin_manager
-    formfield_overrides = {
-        MarkupEditorField: {'widget': MarkupEditorWidget},
-    }
     
     def get_form(self, request, obj=None, **kwargs):
+        """Extend the create form to include initial values."""
         form = super(EntryAdmin, self).get_form(request, obj, **kwargs)
         if obj is None:
+            # Set initial author and site.
             form.base_fields['author'].initial = request.user.pk
             form.base_fields['sites'].initial = (settings.SITE_ID,)
         return form
