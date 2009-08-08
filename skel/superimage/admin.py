bbb15992835c0f7d7da9d866f4adb15b7f30bab5
Greg Thornton <xdissent@gmail.com>
2009-08-08 00:27:34 +0000

    Removed old superimage app

--- skel/superimage/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.db import models
-from skel.superimage.models import Thumbnail
-
-class ThumbnailAdmin(admin.ModelAdmin):
-    pass
-    
-admin.site.register(Thumbnail, ThumbnailAdmin)
