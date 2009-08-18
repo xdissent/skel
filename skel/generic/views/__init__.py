7aa7ce41300bcad7711be70b5a3fa45d2f766731
Greg Thornton <xdissent@gmail.com>
2009-08-18 22:48:36 +0000

    Added generic app

---
+++ skel/generic/views/__init__.py
@@ -0,0 +1,6 @@
+from skel.generic.views.base import GenericView
+from skel.generic.views.list import ListView
+from skel.generic.views.detail import DetailView
+from skel.generic.views.dates import (ArchiveView, YearView, MonthView,
+                                      WeekView, DayView, TodayView,
+                                      DateDetailView)
---
+++ skel/generic/views/edit.py
@@ -0,0 +1,13 @@
+from skel.generic.views.detail import DetailView
+
+class FormView(DetailView):
+    pass
+    
+class CreateView(FormView):
+    pass
+    
+class UpdateView(FormView):
+    pass
+    
+class DeleteView(DetailView):
+    pass
